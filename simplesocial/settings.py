4330033d375abbf7926b9c99a47185833beb8ceb
Airypy <ashutosh.airy@gmail.com>
2020-07-07 11:37:47 +0530

    edit

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
